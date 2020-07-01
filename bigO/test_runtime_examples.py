8540a370f1c825b8ff243431ed0c485875d9a270
katerinaboyer <katerina.a.boyer@gmail.com>
2020-06-30 20:49:15 -0500

    Delete test_runtime_examples.py

--- bigO/test_runtime_examples.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-def test_list_sum_product():
-    assert sum_product([1,2,3,4]) == 34, "should be 34"
-
-if __name__ == "__main__":
-    test_list_sum_product()
-    print("all tests passed")
